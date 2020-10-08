bf19ed662160012f47581d329f5a07532f64924a
fridzse <fridzse.jarmasson@laposte.net>
2020-10-08 12:10:35 +0200

    Setting-up

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { BannerAd, BannerAdSize, TestIds } from '@react-native-firebase/admob';
+import admob, { MaxAdContentRating } from '@react-native-firebase/admob';
 
 class App extends React.Component {
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>HELLO WORLD</Text>
+        <Text style={styles.text}>Ad ! or not</Text>
       </View>
     );
   }
@@ -24,3 +26,19 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+admob()
+  .setRequestConfiguration({
+    // Update all future requests suitable for parental guidance
+    maxAdContentRating: MaxAdContentRating.PG,
+
+    // Indicates that you want your content treated as child-directed for purposes of COPPA.
+    tagForChildDirectedTreatment: true,
+
+    // Indicates that you want the ad request to be handled in a
+    // manner suitable for users under the age of consent.
+    tagForUnderAgeOfConsent: true,
+  })
+  .then(() => {
+    // Request config successfully set!
+  });
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-#Création du squelette de l'application.
-/npx react-native init myapp --template react-native-template-typescript/
+# Création du squelette de l'application.
+/ npx react-native init myapp --template react-native-template-typescript /
+
+# Installation de AdMob
+Installation de firebase avec : 
+/ npm install --safe @react-native-firebase/app /
+L'installation nécessite l'installation de "superstruct" pour continuer : 
+/ npm install --safe superstruct /
+Une fois l'installation compléter c'est au tour de AdMob : 
+/ npm install --safe @react-native-firebase/admob /
+
+# Création d'un compte AdMob
