e28218c68437731ff307baa8c15a61174f97e973
fridzse <fridzse.jarmasson@laposte.net>
2020-10-09 13:19:28 +0200

    Clip it and Ship it

--- App.tsx
+++ App.tsx
@@ -1,27 +1,80 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { createRef } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, Switch, ImageBackground, Alert } from 'react-native';
 import { BannerAd, BannerAdSize, TestIds } from '@react-native-firebase/admob';
 import admob, { MaxAdContentRating } from '@react-native-firebase/admob';
 
+const adUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-3629798182288364/6621972884';
+
+const image = { uri: "https://picsum.photos/300/300" };
+
 class App extends React.Component {
+
+  click = () => {
+    console.log('this is a log.');
+    alert('You\'ve push the button congratulation!');
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Ad ! or not</Text>
+      <View>
+        <View>
+          <ImageBackground source={image} style={styles.picture}>
+            <BannerAd
+              unitId={TestIds.BANNER}
+              size={BannerAdSize.FULL_BANNER}
+              requestOptions={{
+                requestNonPersonalizedAdsOnly: true,
+              }}
+            />
+            <Text style={styles.title}>myapp</Text>
+            <View>
+              <TouchableHighlight style={styles.button} onPress={this.click}>
+                <Text style={styles.textbutton}>redacted</Text>
+              </TouchableHighlight>
+            </View>
+            <Text style={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sagittis ultricies sodales. Donec quis nibh turpis. Sed dictum ornare feugiat.</Text>
+          </ImageBackground>
+        </View>
+
+
+        <View>
+          <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+          />
+        </View>
       </View>
     );
   }
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+    marginTop: 25
+  },
+  picture: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover"
+  },
+  button: {
+    width: 150,
+    backgroundColor: '#7D678C',
+    padding: 15,
+    borderRadius: 15,
+    alignSelf: 'center',
+    marginTop: 250
+  },
+  textbutton: {
+    textAlign: 'center',
+    fontSize: 20,
   },
   text: {
-    fontSize: 30
+    alignItems: 'center',
+    fontSize: 20,
+    textAlign: 'justify',
+    margin: 25
   }
 });
 
@@ -33,7 +86,7 @@ admob()
     maxAdContentRating: MaxAdContentRating.PG,
 
     // Indicates that you want your content treated as child-directed for purposes of COPPA.
-    tagForChildDirectedTreatment: true,
+    tagForChildDirectedTreatment: false,
 
     // Indicates that you want the ad request to be handled in a
     // manner suitable for users under the age of consent.
--- README.md
+++ README.md
@@ -1,12 +1,28 @@
-# Création du squelette de l'application.
-/ npx react-native init myapp --template react-native-template-typescript /
+# Étapes de développement
 
-# Installation de AdMob
+### Création du squelette de l'application.
+`npx react-native init myapp --template react-native-template-typescript`
+
+### Installation de AdMob
 Installation de firebase avec : 
-/ npm install --safe @react-native-firebase/app /
+`npm install --safe @react-native-firebase/app`
 L'installation nécessite l'installation de "superstruct" pour continuer : 
-/ npm install --safe superstruct /
+`npm install --safe superstruct`
 Une fois l'installation compléter c'est au tour de AdMob : 
-/ npm install --safe @react-native-firebase/admob /
+`npm install --safe @react-native-firebase/admob`
 
-# Création d'un compte AdMob
+### Création d'un compte AdMob
+Création de notre projet sur Firebase.
+Création d'une application sur AdMob.
+Création de notre annonce publicitaire.
+Récupération de nos clés RSA : 
+- [ ]  ID d'application `ca-app-pub-3629798182288364~3313283267`
+- [ ]  ID d'annonce `ca-app-pub-3629798182288364/6621972884`
+
+### Configuration de Firebase
+Modification de `/android/build.gradle`.
+Modification de `/android/app/build.gradle`.
+
+### Rebuild de l'application
+`npx react-native run-android`
+Une dépendance est nécessaire pour procéder `multiDexEnabled true`.
